5b19b582ec7b2cbf93ef91603d03252cc14c882a
Elijah Wines <wines.elijah@gmail.com>
2023-01-30 20:13:14 -0500

    finished algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,7 +1,28 @@
 
 
 
-class SelectionSort {
+public class SelectionSort {
+
+    void sort(int arr[]){
+        int n = arr.length;
+
+        // One by one iterate through unsorted list
+        for (int i = 0; i < n-1; i++) {
+
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
 
     public static void main(String[] args) {
